d78daf239e8c0836162254ac240f0885d4ce074f
Gehad Hisham <Gehad_Hisham@Tascairo.com>
2025-01-14 14:49:02 +0200

    add user service and logger

--- src/app/header/admin/admin.component.ts
+++ src/app/header/admin/admin.component.ts
@@ -1,12 +1,24 @@
 import { Component, Inject } from '@angular/core';
+import { UserService } from '../../Services/user.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
 })
 export class AdminComponent {
+  constructor(private userService: UserService) {}
+
   name: string = '';
   gender: string = 'Male';
   subType: string = 'Yearly';
   status: string = 'Active';
+
+  onCreateUser() {
+    this.userService.CreateUser(
+      this.name,
+      this.gender,
+      this.subType,
+      this.status
+    );
+  }
 }
--- src/app/header/admin/user-list/user-list.component.ts
+++ src/app/header/admin/user-list/user-list.component.ts
@@ -1,7 +1,12 @@
 import { Component, Inject } from '@angular/core';
+import { User } from '../../../Models/user';
+import { UserService } from '../../../Services/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
 })
-export class UserListComponent {}
+export class UserListComponent {
+  constructor(private userService: UserService) {}
+  userList = this.userService.GetAllUsers();
+}
