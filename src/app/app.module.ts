cd8b33d5cea6a334b266e1046fa513023c6d6568
Gehad Hisham <Gehad_Hisham@Tascairo.com>
2025-04-29 13:20:29 +0300

    init app, make components, data pinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,20 @@
 import { NgModule, InjectionToken } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './header/home/home.component';
-import { AdminComponent } from './header/admin/admin.component';
-import { HeroComponent } from './header/home/hero/hero.component';
-import { SidebarComponent } from './header/home/sidebar/sidebar.component';
-import { UserListComponent } from './header/admin/user-list/user-list.component';
-import { FormsModule } from '@angular/forms';
-import { UserDetailComponent } from './header/admin/user-detail/user-detail.component';
-import { SubscribeService } from './Services/subscribe.service';
-import { UserService } from './Services/user.service';
-import { LoggerService } from './Services/logger.service';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    AdminComponent,
-    HeroComponent,
-    SidebarComponent,
-    UserListComponent,
-    UserDetailComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [SubscribeService, UserService, LoggerService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,26 +1,7 @@
 import { Component } from '@angular/core';
-import { SubscribeService } from '../Services/subscribe.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  selectedTab: string = 'home';
-
-  constructor(private subscribeService: SubscribeService) {}
-
-  //When HOME Link is clicked
-  HomeClicked() {
-    this.selectedTab = 'home';
-  }
-
-  //When Admin Link is clicked
-  AdminClicked() {
-    this.selectedTab = 'admin';
-  }
-
-  onSubscribe() {
-    this.subscribeService.OnSubscribeClicked('monthly');
-  }
-}
+export class HeaderComponent {}
